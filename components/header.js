9b9ec859491950e98afc2635925063740e6839ea
Akshat <akshatjain5555@gmail.com>
2018-12-06 23:16:05 +0000

    Changes in header file

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import {View , Text , StyleSheet, Platform} from 'react-native';
 const Header = () => {
     return(
         <View style={styles.container}>
-        <Text style={styles.header}>CryptoGazer</Text>
+        <Text style={styles.header}>CryptoGazer App</Text>
         </View>
     )
 }
