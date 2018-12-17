b7d7d2960862ddbcce7a49b3a2a8f720ec268a9f
Ryan Brodigan <brianbrodigan@hotmail.com>
2018-12-17 23:29:37 +0000

    - Added a constants file for APIKeys - Moved duplicate code for parsing JSON to a function - Setup Basic Routes in the News API for fetching Crypto-Related news

--- Server/routes/coinAPI.js
+++ Server/routes/coinAPI.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const https = require('https');
 const router = express.Router();
-const axios = require('axios');
 
+/**
+ *
+ */
 router.get('/coins', (req, res) => {
   https.get('https://api.coinmarketcap.com/v1/ticker/?limit=25', (resp) => {
-      let data = '';
-
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-          data += chunk;
-      });
-
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-          res.send(JSON.parse(data));
-      });
-
+      parseResponse(resp, res)
   }).on('error', (error) => {
       console.log('Error -' + error.message);
   });
@@ -24,27 +15,25 @@ router.get('/coins', (req, res) => {
 
 router.get('/coins/:name', (req, res) => {
   const requestedCoinName = req.params.name;
-  https.get('https://min-api.cryptocompare.com/data/price?fsym='+requestedCoinName+'&tsyms=USD,JPY,EUR', (resp) => {
-    let data = '';
-
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      res.send(JSON.parse(data));
-    });
-
+  https.get(`https://min-api.cryptocompare.com/data/price?fsym=${requestedCoinName}&tsyms=USD,JPY,EUR`, (resp) => {
+      parseResponse(resp, res);
   }).on('error', (error) => {
     console.log('Error -' + error.message);
   });
 });
 
-router.get('/', (req, res) => {
-  res.send('api works');
-});
+function parseResponse(resp, res){
+    let data = '';
 
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        res.send(JSON.parse(data));
+    });
+}
 
 module.exports = router;
--- Server/routes/newsAPI.js
+++ Server/routes/newsAPI.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const https = require('https');
 const router = express.Router();
-const axios = require('axios');
+const apiKeys = require('../APIKeys/APIKeys');
+
+/**
+ * Returns all the top Crypto Headlines
+ */
+router.get('/topCryptoHeadlines', (req, res) => {
+    https.get(`https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=${apiKeys.newsAPIKey}`, (resp) => {
+        parseResponse(resp, res);
+    }).on('error', (error) => {
+        console.log('Error -' + error.message);
+    });
+});
+
+/**
+ * Returns all recent Crypto Headlines
+ */
+router.get('/allCryptoHeadlines', (req, res) => {
+   https.get(`https://newsapi.org/v2/everything?sources=crypto-coins-news&apiKey=${apiKeys.newsAPIKey}`, (resp) => {
+        parseResponse(resp, res);
+   }).on('error', (error) => {
+       console.log('Error -' + error.message);
+   });
+});
+
+function parseResponse(resp, res){
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        res.send(JSON.parse(data));
+    });
+}
 
 module.exports = router;
