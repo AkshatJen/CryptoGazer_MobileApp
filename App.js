85f937236a4fbe74d2d0c33139e2d459557f298b
Akshat <akshatjain5555@gmail.com>
2019-01-22 18:19:21 +0000

    icons , iosSplash , navigation

--- App.js
+++ App.js
@@ -3,15 +3,29 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import Store from './Store';
 import FlatListExample from './components/FlatListExample';
+import {createStackNavigator , createAppContainer , createBottomTabNavigator} from 'react-navigation';
 
 import { Header, CryptoContainer } from './components';
+
+
 
 export default class App extends Component{
   render() {
     return (
+     <AppContainer/>
+    );
+  }
+}
+class Coins extends Component{
+  static navigationOptions = {
+  
+  }
+  render() {
+    return (
       <Provider store={Store}>
       <View style={styles.container}>
-      <Header style={styles.header}/> 
+     { /*<Header style={styles.header}/> */ 
+      }
       <CryptoContainer />
       {/*<FlatListExample /> */}
       </View>
@@ -19,6 +33,66 @@ export default class App extends Component{
     );
   }
 }
+
+class Feed extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Feed</Text>
+      </View>
+    );
+  }
+}
+class Settings extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Settings</Text>
+      </View>
+    );
+  }
+}
+
+const DashboardTabNavigator = createBottomTabNavigator(
+  {
+    Feed,
+    Coins,
+    Settings
+  },
+  {
+    navigationOptions: ({ navigation }) => {
+      const { routeName } = navigation.state.routes[navigation.state.index];
+      return {
+        headerTitle: routeName
+      };
+      
+    },
+    tabBarOptions: {
+      allowFontScaling : true,
+      activeTintColor: 'white',
+      inactiveTintColor: 'gray',
+      labelStyle:{
+        fontSize :16
+      },
+      style: {
+        backgroundColor: '#262A4A',
+      },
+    },
+  }
+);
+
+const AppStackNavigator = createStackNavigator({
+  Home : DashboardTabNavigator, },
+  {
+    defaultNavigationOptions:{
+        headerStyle : {
+          backgroundColor : '', // #262A4A
+          
+        },
+    
+    }
+  }
+  );
 
 const styles = StyleSheet.create({
   container: {
@@ -29,3 +103,5 @@ const styles = StyleSheet.create({
   },
  
 });
+
+const AppContainer = createAppContainer(AppStackNavigator);
