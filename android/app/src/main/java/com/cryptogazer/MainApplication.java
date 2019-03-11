fa825c224da9fd2e236e81e6aa2bf703efce2788
Ryan Brodigan <brianbrodigan@hotmail.com>
2019-03-11 16:51:45 +0000

    - Removed React Native Facebook Login from dependencies - Changed the Constant value for the API to point to Heroku App, hosting the Node API

--- android/app/src/main/java/com/cryptogazer/MainApplication.java
+++ android/app/src/main/java/com/cryptogazer/MainApplication.java
@@ -3,7 +3,6 @@ package com.cryptogazer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.magus.fblogin.FacebookLoginPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FacebookLoginPackage(),
             new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
             new RNFirebaseAuthPackage()
